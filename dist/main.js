cd1d56e4031787a750f72659fc54f3aec3fb2dd4
Jeff Redbeard <jeffredbeard7@gmail.com>
2022-04-02 11:53:43 -0500

    Moved Console Commands to Separate Module

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,7 @@ const { TaskEngine } = require('./task');
 const { Stats } = require('./stats');
 const { logging } = require('./logging');
 const { guimenu } = require('./guimenu');
+const cmds = require('./cmds');
 
 module.exports.rooms = {};
 
@@ -13,142 +14,15 @@ module.exports.loop = function () {
     return;
   }
 
+  cmds.register();
+
   let rooms = {};
   module.exports.rooms = rooms;
 
-  /*
-  let extra_config = {
-    'W24S41': {
-        claimteam: [
-          {
-            tr: 'W24S38',
-          },
-        ], 
-        warfare_small: [
-          {
-            uid: 'ironspear',
-            room: 'W24S38',
-            max_level: 6,
-            count: 1,
-          },
-        ],
-        'remote_sources': [
-          {
-            uid: 'apple',
-            mining_level: 3,
-            hauling_power: 5,
-            sources: [
-              {
-                sid: '5bbcb06f9099fc012e63c2ce',
-                room: 'E56S33',
-              },
-            ],
-          },
-          {
-            uid: 'grape',
-            mining_level: 3,
-            hauling_power: 5,
-            sources: [
-              {
-                sid: '5bbcb0819099fc012e63c486',
-                room: 'E57S32',
-              },
-            ],
-          },
-        ],
-    },
-  };
-  */
-
-  Memory.ecfg = Memory.ecfg || {};
-  const ecfg = Memory.ecfg;
-
-  Game.ops_create_claim_team = (home_room, target_room, allowable_rooms) => {
-    const op = { 
-      tr: target_room,
-      ar: allowable_rooms,
-    };
-    ecfg[home_room] = ecfg[home_room] || {};
-    ecfg[home_room].claimteam = ecfg[home_room].claimteam || [];
-    ecfg[home_room].claimteam.push(op);
-    return 'Added claim operation.'; 
-  };
-
-  Game.ops_create_warfare_small = (home_room, uid, room, max_level, count) => {
-    const op = {
-      uid: uid,
-      room: room,
-      max_level: max_level,
-      count: count,
-    };
-
-    ecfg[home_room] = ecfg[home_room] || {};
-    ecfg[home_room].warfare_small = 
-      ecfg[home_room].warfare_small || [];
-    ecfg[home_room].warfare_small.push(op);
-  };
-
-  Game.ops_del = (hroom, mod, index) => {
-    ecfg[hroom][mod].splice(index, 1);
-    return `Deleted op of room ${hroom} with mod ${mod} on index ${index}.`
-  };
-
-  Game.ops_list = () => {
-    console.log('== OPS CON [LIST] ==');
-    for (let hroom in ecfg) {
-      for (let mod in ecfg[hroom]) {
-        for (let ndx = 0; ndx < ecfg[hroom][mod].length; ++ndx) {
-          let unit = ecfg[hroom][mod][ndx];
-          if (mod === 'warfare_small') {
-            console.log(`[${hroom}/${mod}/${ndx}] uid=${unit.uid} room=${unit.room} max_level=${unit.max_level} count=${unit.count}`);
-          } else {
-            console.log(`[${hroom}/${mod}/${ndx}] <unimplemented parameter display>`);
-          }
-        }
-      }
-    }
-  };
-
-  Game.log_rule_list = () => {
-    console.log('== LOG RULES ==');
-    const logrules = Memory.logrules || [];
-    for (let x = 0; x < logrules.length; ++x) {
-      const rule = logrules[x];
-      console.log(`[${x}] ${rule}`);
-    }
-    return 'End of List';
-  };
-
-  Game.logging = logging;
-
-  Game.log_rules_clear = () => {
-    Memory.logrules = [];
-    return 'Cleared All Log Rules';
-  };
-
-  Game.log_print_known_groups = () => {
-    const kgroups = logging.get_known_groups();
-    console.log('== KNOWN LOG GROUPS ==');
-    logging.get_known_groups().forEach(gtext => {
-      console.log(gtext);
-    });
-    return 'Done';
-  };
-
-  Game.log_rule_del = (ndx) => {
-    const logrules = Memory.logrules || [];
-    const rule = logrules[ndx];
-    logrules.splice(ndx, 1);
-    return `Removed log rule ${ndx}: ${rule}`;
-  }; 
-
-  Game.log_rule_add = (rule) => {
-    const logrules = Memory.logrules || [];
-    logrules.push(rule);
-    return `Added log rule: ${rule}`;
-  };
-
   let creep_deaths = [];
+  
+  Memory.ecfg = Memory.ecfg || {};
+  let ecfg = Memory.ecfg; 
 
   logging.info('creating rooms');
   for (let name in game.rooms()) {
